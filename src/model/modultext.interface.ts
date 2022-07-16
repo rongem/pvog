c81a50472742b54492215fa1957671aca8cae716
Michael Ronge <github@ronge.org>
2022-07-16 06:41:05 +0200

    changed text handling

--- src/model/modultext.interface.ts
+++ src/model/modultext.interface.ts
@@ -2,14 +2,14 @@ import { createID } from './id.model';
 import { RestLeistung } from './rest/leistung.model';
 
 export const createText = (leistung: RestLeistung): IModultext[] => leistung.modulText.map(t => {
-    if (t.inhalt) {
+    if (t.inhalt && t.inhalt.length > 0) {
         return t.inhalt.map(i => {
-            let text = i.text.replace(/<\/?[^>]+(>|$)/g, '').trim();
-            text = text.replace(/\s/g, ' ').replace(/  +/g, ' ');
+            let text = i.text.replace(/<\/?[^>]+(>|$)/g, '').replace(/^-/, '').trim();
+            text = text.replace(/\s/g, ' ').replace(/&#xa0;/g, ' ').replace(/  +/g, ' ');
             const zeichenAnzahl = text.length;
             const wortAnzahl = text.split(' ').length;
             if (zeichenAnzahl > 70) {
-                text = text.substring(0, text.indexOf(' ', 25) + 1) + '[...]' + text.substring(text.indexOf(' ', zeichenAnzahl - 30) - 1);
+                text = text.substring(0, text.indexOf(' ', 25) + 1) + '[...]' + text.substring(text.indexOf(' ', zeichenAnzahl - 30));
             }
             return {
                 id: createID(leistung.id),
